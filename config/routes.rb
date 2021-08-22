f8963b26034f9053d8be3871c534d1581f4d5847
tatsuro1997 <minagawa@dptr.jp>
2021-08-23 08:53:56 +0900

    ユーザ管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  root to: 'blogs#index'
+
+  devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'
+}
+
+devise_scope :user do
+  get "user/:id", :to => "users/registrations#detail"
+  get "signup", :to => "users/registrations#new"
+  get "login", :to => "users/sessions#new"
+  get "logout", :to => "users/sessions#destroy"
+end
+
   resources :blogs do
     resources :comments
   end
