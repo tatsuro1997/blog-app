58c9ccab8895c11f34c8f12cafafe111b6bde3e2
tatsuro1997 <minagawa@dptr.jp>
2021-08-30 09:41:12 +0900

    tag機能の実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,13 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
+    @tag_list = Tag.all
     @blogs = Blog.all
   end
 
   # GET /blogs/1 or /blogs/1.json
   def show
+    @blog_tags = @blog.tags
   end
 
   # GET /blogs/new
@@ -25,9 +27,11 @@ class BlogsController < ApplicationController
   # POST /blogs or /blogs.json
   def create
     @blog = Blog.new(blog_params)
+    tag_list = params[:blog][:tag_name].split(nil)
 
     respond_to do |format|
       if @blog.save
+        @blog.save_tag(tag_list)
         format.html { redirect_to @blog, notice: "Blog was successfully created." }
         format.json { render :show, status: :created, location: @blog }
       else
@@ -58,6 +62,12 @@ class BlogsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def search
+    @tag_list = Tag.all
+    @tag = Tag.find(params[:tag_id])
+    @blogs = @tag.blogs.all
+end
 
   private
 
