54aec22ca7d50d69c1a1d5b5792058c9dcb2108a
tatsuro1997 <minagawa@dptr.jp>
2021-09-06 09:21:52 +0900

    admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: "admin@example.jp",
+             password:  "admin1997",
+             password_confirmation: "admin1997",
+             admin: true)
